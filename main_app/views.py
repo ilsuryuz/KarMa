c4c5acda4e36afa541a002d094a28c6dc64c9332
Ilsur <yuzlikeyevi@gmail.com>
2022-10-26 01:25:14 -0500

    About, Welcome, page protected routes

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Profile
 from .forms import ProfileForm
@@ -15,15 +17,17 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def posts_index(request):
     posts = Post.objects.all()
     return render(request, 'posts/index.html', { 'posts': posts})
 
+@login_required
 def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'posts/detail.html', { 'post': post })
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
   model = Post
   fields = ['year', 'mark', 'model', 'content', 'photo']
   
@@ -32,11 +36,11 @@ class PostCreate(CreateView):
       
       return super().form_valid(form)
   
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['year', 'mark', 'model', 'content', 'photo']
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = '/posts/'
 
@@ -56,7 +60,7 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message }
     return render(request, 'registration/signup.html', context)
-
+@login_required
 def profile(request):
     profile = request.user.profile
     profile_form = ProfileForm(instance=profile)
